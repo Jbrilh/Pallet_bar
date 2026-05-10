28006e6b33860d57904cbbb900e399a34901b75d
Jbril <Jbrilhdeveloper@gmail.com>
2026-05-10 15:30:41 +0300

    rail way updated

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const { createClient } = require('@supabase/supabase-js')
 // --------------------
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    'https://gzwxcdjvezevyvicamgc.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d3hjZGp2ZXpldnl2aWNhbWdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTgxMzYsImV4cCI6MjA5Mzk3NDEzNn0.-jQhDC2o6Pbb-Nk8Byhs2lMzGks637KwH-6NXsb2Ujg'
 )
 
 // --------------------
